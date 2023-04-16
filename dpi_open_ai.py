76ff4c42a04031c0beff4bf06c239eb3ba77ff83
Praneeth Ponnekanti <praneethcs23@gmail.com>
2023-04-17 04:08:47 +0530

    Update dpi_open_ai.py

--- dpi_open_ai.py
+++ dpi_open_ai.py
@@ -3,20 +3,11 @@ import openai
 import pandas as pd
 from datetime import datetime, timedelta
 
-# Set up OpenAI API credentials
-openai.api_key = "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"  # Replace with your actual OpenAI API key
-
-# Phases of the consumer journey
-phases = [
-    "Initial Consideration",
-    "Active Evaluation",
-    "Moment of Purchase",
-    "Post-Purchase",
-    # Add more phases as needed
-]
-
 # Function to generate offerings and associated keywords using OpenAI API
-def generate_offerings_and_keywords(phase, company_name):
+def generate_offerings_and_keywords(api_key, phase, company_name):
+    # Set up OpenAI API credentials
+    openai.api_key = api_key
+    
     # TODO: Replace with your own logic to generate offerings and keywords using OpenAI API
     offerings = openai.Completion.create(
         prompt=f"Generate offerings for {phase} phase of consumer journey for {company_name}",
@@ -46,18 +37,28 @@ def generate_google_search_url(phase, company_name, start_date, end_date):
 def main():
     st.title("Consumer Journey and Offerings")
 
+    # Input for OpenAI API key
+    api_key = st.text_input("OpenAI API Key:", type="password")
+
     # Input for phase of consumer journey
+    phases = [
+        "Initial Consideration",
+        "Active Evaluation",
+        "Moment of Purchase",
+        "Post-Purchase",
+        # Add more phases as needed
+    ]
     phase = st.selectbox("Phase of Consumer Journey:", phases)
 
     # Input for company name
-    company_name = st.text_input("Company Name:", value="", key="company_name_input")
+    company_name = st.text_input("Company Name:", value="")
 
     # Input for start date and end date for search results filtering
-    start_date = st.date_input("Start Date:", value=(datetime.now() - timedelta(days=365)), key="start_date_input")
-    end_date = st.date_input("End Date:", value=datetime.now(), key="end_date_input")
+    start_date = st.date_input("Start Date:", value=(datetime.now() - timedelta(days=365)))
+    end_date = st.date_input("End Date:", value=datetime.now())
 
     # Generate offerings and keywords
-    offerings, keywords = generate_offerings_and_keywords(phase, company_name)
+    offerings, keywords = generate_offerings_and_keywords(api_key, phase, company_name)
 
     # Generate Google search URL with date range filter
     google_search_url = generate_google_search_url(phase, company_name, start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
