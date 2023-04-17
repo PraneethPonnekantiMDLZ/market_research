4f763f44f85a2530268f259c892aa7f29ed0f5f8
Praneeth Ponnekanti <praneethcs23@gmail.com>
2023-04-17 17:54:58 +0530

    Update dpi_open_ai.py

--- dpi_open_ai.py
+++ dpi_open_ai.py
@@ -89,26 +89,83 @@ def main():
     st.write("Output:")
     st.markdown('<div style="overflow-x:auto; margin-top: 20px;">', unsafe_allow_html=True)
     # Display generated keywords separately
+    def main():
+    #st.title("DPI using OpenAI") # Update with an appropriate title
+    st.set_page_config(page_title="DPI using OpenAI", page_icon=":rocket:", layout="wide") # Update with appropriate title and icon
+    
+    # Create collapsible input bar
+    with st.beta_expander("Input", expanded=True):
+        # Input for OpenAI API key
+        api_key = st.text_input("OpenAI API Key:", type="password")
+        
+        # Input for company name
+        company_name = st.text_input("Company Name:", value="")
+
+        # Input for business dimension
+        business_dimension = st.selectbox("Business Dimension:", options=["Commercial Optimization",
+                                            "Consumer Experience",
+                                            "Consumer Insight",
+                                            "Customer Experience",
+                                            "Digital Back Office",
+                                            "Digital Commerce",
+                                            "Digital Marketing",
+                                            "Innovation & Smart R&D",
+                                            "Smart Supply Chain & Manufacturing",
+                                            "Workforce of the future"]
+                                         )
+        
+        # Input for statement
+        statement = st.text_area("Statement:", value="")
+        
+        # Input for custom keywords
+        custom_keywords = st.text_input("Custom Keywords (comma-separated):", value="")
+        
+        # Input for start date
+        start_date = st.date_input("Start Date:", value=(datetime.now() - timedelta(days=365)).date())
+
+        # Input for end date
+        end_date = st.date_input("End Date:", value=datetime.now().date())
+        
+        # Button to generate output
+        generate_button = st.button("Generate Output")
+           
+    # Output section with full screen width
+    st.markdown('<div class="collapsible-content"></div>', unsafe_allow_html=True)
+    st.write("Output:")
+    st.markdown('<div style="overflow-x:auto; margin-top: 20px;">', unsafe_allow_html=True)
+    # Display generated keywords separately
     if generate_button:
         if statement and company_name and business_dimension and start_date and end_date:
-            keywords = generate_keywords(statement, company_name, business_dimension, start_date, end_date, custom_keywords)
-            st.markdown(f"**Generated Keywords:**\n{', '.join(keywords)}")
+            generated_keywords = generate_keywords(statement, company_name, business_dimension, start_date, end_date)
+            st.markdown(f"**Generated Keywords:**\n{', '.join(generated_keywords)}")
             st.markdown('<br>', unsafe_allow_html=True)
+            
+            if custom_keywords:
+                custom_keywords_list = [keyword.strip() for keyword in custom_keywords.split(",")]
+                st.markdown(f"**Custom Keywords:**\n{', '.join(custom_keywords_list)}")
+                st.markdown('<br>', unsafe_allow_html=True)
     
-    # Display Google search URL with date range filter
+    # Display Google search URL with date range filter for generated keywords
     if generate_button : 
         if statement and company_name and business_dimension and start_date and end_date:
-            google_search_url = generate_google_search_url(statement, company_name, business_dimension, start_date, end_date)
-            st.markdown(f"**Google Search URL with Date Range Filter:**\n{google_search_url}")
+            google_search_url_generated = generate_google_search_url(statement, company_name, business_dimension, start_date, end_date)
+            st.markdown(f"**Google Search URL with Date Range Filter (Generated Keywords):**\n{google_search_url_generated}")
             st.markdown('<br>', unsafe_allow_html=True)
+            
+            if custom_keywords:
+                google_search_url_custom = generate_google_search_url(statement, company_name, business_dimension, start_date, end_date, custom_keywords_list)
+                st.markdown(f"**Google Search URL with Date Range Filter (Custom Keywords):**\n{google_search_url_custom}")
+                st.markdown('<br>', unsafe_allow_html=True)
     
-    # Display table with search results
+    # Display table with search results for generated keywords
     if generate_button : 
-        if statement and company_name and business_dimension and start_date and end_date:
-            results = get_search_results(google_search_url)
-            st.write(results)
+           if statement and company_name and business_dimension and start_date and end_date:
+                df = generate_search_results(statement, company_name, business_dimension, start_date, end_date)
+                st.write(df)
     
     st.markdown('</div>', unsafe_allow_html=True)
+
+
 
 
 if __name__ == "__main__":
