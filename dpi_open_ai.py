3a821d6dc4b75620b6b8c81d1be1a5162a44e008
Praneeth Ponnekanti <praneethcs23@gmail.com>
2023-04-17 10:34:16 +0530

    Update dpi_openai.py

--- dpi_open_ai.py
+++ dpi_open_ai.py
@@ -4,13 +4,13 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 # Function to generate offerings and associated keywords using OpenAI API
-def generate_offerings_and_keywords(api_key, phase, company_name):
+def generate_offerings_and_keywords(api_key, statement, company_name, business_dimension):
     # Set up OpenAI API credentials
     openai.api_key = api_key
     
     # TODO: Replace with your own logic to generate offerings and keywords using OpenAI API
     offerings = openai.Completion.create(
-        prompt=f"Generate offerings for {phase} phase of consumer journey for {company_name}",
+        prompt=f"Generate offerings for {business_dimension} {statement} for {company_name}",
         engine="text-davinci-002",
         max_tokens=1024,
         n=1,
@@ -18,7 +18,7 @@ def generate_offerings_and_keywords(api_key, phase, company_name):
         temperature=0.5
     )
     keywords = openai.Completion.create(
-        prompt=f"Generate keywords for {phase} phase of consumer journey for {company_name}",
+        prompt=f"Generate keywords for {business_dimension} {statement} for {company_name}",
         engine="text-davinci-002",
         max_tokens=1024,
         n=1,
@@ -28,43 +28,45 @@ def generate_offerings_and_keywords(api_key, phase, company_name):
     return offerings.choices[0].text.strip(), keywords.choices[0].text.strip()
 
 # Function to generate Google search URL with date range filter
-def generate_google_search_url(phase, company_name, start_date, end_date):
-    # TODO: Replace with your own logic to generate Google search URL with appropriate date range filter
-    url = f"https://www.google.com/search?q={company_name} {phase} phase of consumer journey&date={start_date}:{end_date}"
+def generate_google_search_url(statement, company_name, business_dimension, start_date, end_date):
+    # Format the company name with double quotes
+    company_name = f'"{company_name}"'
+    
+    # Format the dates with after: and before: parameters
+    dates = f'after:{start_date.strftime("%Y-%m-%d")}%20before:{end_date.strftime("%Y-%m-%d")}'
+    
+    # Generate the Google search URL
+    url = f"https://www.google.com/search?q={business_dimension} {company_name} {statement}&{dates}"
     return url
 
 # Streamlit app
 def main():
-    st.title("Consumer Journey and Offerings")
+    st.title("DPI using OpenAI") # Update with an appropriate title
 
     # Input for OpenAI API key
     api_key = st.text_input("OpenAI API Key:", type="password")
 
-    # Input for phase of consumer journey
-    phases = [
-        "Initial Consideration",
-        "Active Evaluation",
-        "Moment of Purchase",
-        "Post-Purchase",
-        # Add more phases as needed
-    ]
-    phase = st.selectbox("Phase of Consumer Journey:", phases)
+    # Input for statement
+    statement = st.text_area("Statement:", value="")
 
     # Input for company name
     company_name = st.text_input("Company Name:", value="")
+
+    # Input for business dimension
+    business_dimension = st.text_input("Business Dimension:", value="")
 
     # Input for start date and end date for search results filtering
     start_date = st.date_input("Start Date:", value=(datetime.now() - timedelta(days=365)))
     end_date = st.date_input("End Date:", value=datetime.now())
 
     # Generate offerings and keywords
-    offerings, keywords = generate_offerings_and_keywords(api_key, phase, company_name)
+    offerings, keywords = generate_offerings_and_keywords(api_key, statement, company_name, business_dimension)
 
     # Generate Google search URL with date range filter
-    google_search_url = generate_google_search_url(phase, company_name, start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
+    google_search_url = generate_google_search_url(statement, company_name, business_dimension, start_date, end_date)
 
     # Display the generated data in a DataFrame
-    data = {"Phase of Consumer Journey": [phase], f"{company_name} Offerings": [offerings], "Keywords": [keywords], "Google Search URL (Past Year)": [google_search_url]}
+    data = {"Statement": [statement], "Company Name": [company_name], "Business Dimension": [business_dimension], "Offerings": [offerings], "Keywords": [keywords], "Google Search URL (Date Range)": [google_search_url]}
     df = pd.DataFrame(data)
     st.table(df)
 
